e2f2ab003e1a8f1818c88e93608a76c59ce86468
ayela005 <ayela005@hotmail.com>
2024-10-16 18:05:26 +0200

    oppgave 1

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,8 +4,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		//Opprett et GPSPoint-objekt 
 		
+	GPSPoint punkt = new GPSPoint(1, 2.0, 3.0, 5.0);
+	
+	
+	//Skriv ut tidspunktet ved å bruke getTime() der punkt er det nye navnet.
+	System.out.println( "Tidspunkt: " + punkt.getTime());
+	
+	
+	//Endre tidpunktet til 2 
+	punkt.setTime(2);
+	
+	
+	//skriv ut informasjon om objektet ved bruk toString()
+	System.out.println(" Informasjon om GPSPoint" + punkt.toString());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
